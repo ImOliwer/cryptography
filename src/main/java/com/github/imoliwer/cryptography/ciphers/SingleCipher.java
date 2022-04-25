a8d31bf7c9e10728f7796594b4d7e109372cfd20
imoliwer <oliwer.lindell@hotmail.com>
2022-04-25 17:16:22 +0200

    Replace SingleCipher's implementation to make use of its Mutual basic call.

--- src/main/java/com/github/imoliwer/cryptography/ciphers/SingleCipher.java
+++ src/main/java/com/github/imoliwer/cryptography/ciphers/SingleCipher.java
@@ -4,10 +4,10 @@ import com.github.imoliwer.cryptography.ByteCipher;
 import com.github.imoliwer.cryptography.helper.Converter;
 import com.github.imoliwer.cryptography.helper.Transformer;
 
-import javax.crypto.Cipher;
-import java.security.GeneralSecurityException;
 import java.security.Key;
 import java.util.Set;
+
+import static com.github.imoliwer.cryptography.ciphers.MutualCipherUtil.basic;
 
 /**
  * This implementation of {@link ByteCipher} represents a 'single key' cipher.
@@ -47,31 +47,14 @@ public final class SingleCipher<Type, Options> extends
     /** @see ByteCipher#cipher(int, byte[], Object, Key) **/
     @Override
     public Type cipher(int mode, byte[] handle, Options options, Key secretKey) {
-        if (mode != 1 && mode != 2) {
-            throw new RuntimeException("Mode operation must be 1 (encryption) or 2 (decryption).");
-        }
-
-        try {
-            final Cipher cipher = Cipher.getInstance(this.algorithm);
-            cipher.init(mode, secretKey);
-
-            final Class<? extends Type> conversionType = converter == null ? null : converter.getConversionType();
-            if (conversionType == null) {
-                throw new RuntimeException("Missing converter");
-            }
-
-            byte[] finalized = cipher.doFinal(handle);
-            if (conversionType == byte[].class || conversionType == Byte[].class) {
-                return (Type) finalized;
-            }
-
-            for (Transformer transformer : this.transformers) {
-                finalized = transformer.transform(mode, finalized);
-            }
-
-            return converter.convert(mode, finalized, options);
-        } catch (GeneralSecurityException ex) {
-            throw new RuntimeException(ex);
-        }
+        return basic(
+            mode,
+            handle,
+            options,
+            secretKey,
+            this.algorithm,
+            this.converter,
+            this.transformers
+        );
     }
 }
